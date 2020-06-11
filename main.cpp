3a19c7b003c5919d10ac54f941781edf9a92049e
shplss <vasco.audio@gmail.com>
2020-06-11 08:10:17 +0000

    UDTs converted into code. Added new UDTs.

--- main.cpp
+++ main.cpp
@@ -133,6 +133,27 @@ Thing 1) Recording Studio
     3) test all equipment
  */
 
+struct RecStudio 
+{
+    // number of microphones
+    int numMicrophones = 25;
+    // number of outboard units
+    int numOutboardUnits = 30;
+    // amount of sessions per month
+    int sessionPerMonth = 10;
+    // amount of discount for multiple day sessions
+    float multiDayDiscount = 20.0f;
+    // amount of revenew per month
+    float revenewPerMonth = 6000.0f;
+
+    // record an album
+    void recordAlbum(int numMusicians, int genreCode, int amountTime);
+    // charge sessions to client
+    float chargeSession(int hoursUsed, float discount, float extraCosts = 0.0f);
+    // test all equipment
+    void testAllEquipment();
+};
+
 /*
 Thing 2) Supermarket
 5 properties:
@@ -146,6 +167,43 @@ Thing 2) Supermarket
     2) restock products
     3) bake bread
  */
+
+struct Supermarket 
+{
+    // number of employees per shift
+    int employPerShift = 15;
+    // number of active cashiers
+    int numActiveChashiers = 5;
+    // amount of sales per day
+    float salesPerDay = 10000.0f;
+    // amount of profit per day
+    float profitPerDay = 3000.0f;
+    // list with best sold products
+    int topSellerID = 0;
+
+    struct Product 
+    {
+        int productID = 0;
+        std::string productName = "";
+        float retailPrice = 0.0f;
+        int currentStock = 0;
+        int orderedStock = 0;
+
+        // updates product price
+        bool updatePrice(float newPrice);
+        // predicts needed stock based on previous sales
+        int stockPrediction(int salesPeriodInMonths, int salesAmount, int currentStock);
+        // compares two prices and gives the difference
+        float priceComparison(Product prodToCompareWith);
+    };                       
+
+    // charge client for groceries
+    float chargeClient(float productTotal, float taxAmount = 21.0f, float discount = 0);
+    // restock products
+    bool restockProducts(Product prod, int restockQuantity);
+    // bake bread
+    void bakeBread(int breadType, int bakeQuantity);
+};
 
 /*
 Thing 3) Bakery
@@ -161,6 +219,27 @@ Thing 3) Bakery
     3) calculate food waste
  */
 
+struct Bakery
+{
+    // number of bags of flour
+    int numBagsFlour = 20;
+    // amount of bread per bread type
+    int XBread_Amount = 40;
+    // amount of cake per cake type
+    int XCake_Amount = 5;
+    // amount of sales per day
+    float daySales = 5000.0f;
+    // amount of products to deliver per establishment
+    int XCliente_totalProdDeliver = 5;
+
+    // bake cake
+    void bakeCake(int cakeType, int bakeQuantity);
+    // deliver bread
+    bool deliverBread(int clientID, int orderID, int deliveryTime);
+    // calculate food waste
+    float foodWaste_Kg(int amountDays);
+};
+
 /*
 Thing 4) Bar
 5 properties:
@@ -174,6 +253,27 @@ Thing 4) Bar
     2) charge costumer
     3) order new stock
  */
+
+struct Bar 
+{
+    // number of products in stock per category
+    int XCategory_numProdStock = 200;
+    // number of product to order per category
+    int XCategory_numProdOrder = 50;
+    // amount of profit per day
+    float dayProfit = 4000.0f;
+    // amount of products sold per day
+    int prodSoldPerDay = 150;
+    // specials (dishes) per day
+    int numDailySpecials = 3;
+
+    // serve drink
+    bool serveDrink(int drinkID, int quantity = 1);
+    // charge costumer
+    float billClient(float productTotal, bool hasTabOpen = false, float taxAmount = 9.0f);
+    // order new stock
+    void orderNewStock(int productID, int quantityOrder);
+};
 
 /*
 Thing 5) Master Section
@@ -189,6 +289,27 @@ Thing 5) Master Section
     3) send audio to headphones
  */
 
+struct MasterSection
+{
+    // Master Fader Level in dB
+    float masterFaderLevel = 0.0f;
+    // Speaker Level in dB
+    float monitorLevel = -20.0f;
+    // Monitor Selection
+    int monitorSelID = 0;
+    // Foldback Level in dB
+    float foldbackLevel = -10.0f;
+    // Bus Compressor On/Off
+    bool busCompressor = false;
+
+    // select different set of speakers
+    void selectSpeaker(int monitorSelID);
+    // change monitoring level
+    void changeLevel(float gainChange);
+    // send audio to headphones
+    bool sendMasterToHP(float sendLevel = -20.0f);
+};
+
 /*
 Thing 6) Input Section
 5 properties:
@@ -202,6 +323,27 @@ Thing 6) Input Section
     2) apply hpf on input
     3) attenuate MTR return
  */
+
+struct InputSection
+{
+    // Mic Gain Level in dB
+    float micGain = -80.0f;
+    // Line Level in dB
+    float lineGain = 0.0f;
+    // Input Source
+    int inputSourceID = 0;
+    // Meter Flip
+    bool meterFlip = false;
+    // HPF On/Off
+    bool hpfEngage = false;
+
+    // amplify input signal
+    void gainInput(float gainAmount);
+    // apply hpf on input
+    void engageHPF(bool hpfEngage);
+    // attenuate MTR return
+    void gainMtrRet(float gainAmount);
+};
 
 /*
 Thing 7) Output Section
@@ -217,6 +359,27 @@ Thing 7) Output Section
     3) position signal in stereo field
  */
 
+struct OutputSection
+{
+    // Matrix Output Channel
+    int outputChannel = 0;
+    // Mix Switch On/Off
+    bool addToMix = false;
+    // Output Level
+    float channelFader = -80.0f;
+    // Channel Panning
+    float channelPanning = 0.0f;
+    // Meter Flip
+    bool meterFlip = false;
+
+    // include singnal in subgroup
+    void sendToSubgroup(int subgroupID);
+    // set channel output level
+    void setChannelLevel(int channelID, float channelFader);
+    // position signal in stereo field
+    void changePanning(int channelID, float channelPanning);
+};
+
 /*
 Thing 8) EQ Section
 5 properties:
@@ -230,6 +393,27 @@ Thing 8) EQ Section
     2) increase presence in the signal
     3) decrease low end boominess
  */
+
+struct EQSection
+{
+    // High Shelf Frequency
+    float highShelfFreq = 20000.0f;
+    // High Shelf Gain
+    float highShelfGain = 0.0f;
+    // Parametric Frequency
+    float ParamFreq = 1000.0f;
+    // Parametric Q
+    float ParamQ = 1.0f;
+    // Parametric Gain
+    float ParamGain = 0.0f;
+
+    // remove unwanted frequencies
+    void removeFreq(float freqToRemove, float amountInDb);
+    // increase presence in the signal
+    void addPresence(float cutOffFreq, float amountInDb);
+    // decrease low end boominess
+    void decreaseBoom(float centerFreq, float bandWidth, float amountInDb);
+};
 
 /*
 Thing 9) Dynamics Section
@@ -245,6 +429,27 @@ Thing 9) Dynamics Section
     3) tame signal transients
  */
 
+struct DynamicsSection
+{
+    // Compressor Threshold
+    float compThresh = 0.0f;
+    // Compressor Ratio
+    float compRatio = 2.0f;
+    // Compressor Attack
+    int compAttackMs = 10.0f;
+    // Compressor Release
+    int compRelMs = 200.0f;
+    // Compressor Engage Switch
+    bool compEngage = false;
+
+    // limit signal to avoid clipping
+    float limitSignal(float threshold, int lookaheadMs);
+    // make signal louder
+    void compSignal(float compThresh, float compRatio, float makeUpGain);
+    // tame signal transients
+    void tameSignalTransients(int channelID);
+};
+
 /*
 Thing 10) Mixing Console
 5 properties:
@@ -258,6 +463,43 @@ Thing 10) Mixing Console
     2) process signals individually
     3) choose which speakers to use
  */
+
+struct MixingConsole
+{
+    // Master Section
+    MasterSection MS;
+    // Input Section
+    InputSection IS;
+    // Output Section
+    OutputSection OS;
+    // EQ Section
+    EQSection ES;
+    // Dynamics Section
+    DynamicsSection DS;
+
+    struct ChannelStrip
+    {
+        int channelID = 0;
+        float SF_Level = -80.0f;
+        float LF_Level = -80.0f;
+        bool SF_toMasterBus = false;
+        bool LF_toMasterBus = false;
+        
+        // resets channelstrip
+        bool resetChannel();
+        // copies from sf to lf or vice-versa
+        bool copySettingsFromPath(std::string pathToCopy);
+        // change channel position
+        void changeChannelID(int oldID, int newID);
+    };
+
+    // mix signals together
+    void mixAllActiveChannels();
+    // process signals individually
+    void processChannel(ChannelStrip chStrp, DynamicsSection dynSec, EQSection eqSec, bool dynBeforeEq = true);
+    // choose which speakers to use
+    void selectMainMonitors(int monitorID, float gainTrim = 0.0f);
+};
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
